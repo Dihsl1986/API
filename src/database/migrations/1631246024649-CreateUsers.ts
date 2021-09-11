433e6a03b348706149890b28e413b43e9142562e
Diego Santos Lima <dihsl1986@gmail.com>
2021-09-11 01:38:07 -0300

    Atualizando criando tabela 36min

--- src/database/migrations/1631246024649-CreateUsers.ts
+++ src/database/migrations/1631246024649-CreateUsers.ts
@@ -1,11 +1,40 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
 
 export class CreateUsers1631246024649 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "users",
+                columns: [
+                    {
+                        name: "id",
+                        type: "uuid",
+                        isPrimary: true,
+                    },
+                       
+                    {
+                        name: "name",
+                        type: "varchar",
+                    },
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
+                    {
+                        name: "email",
+                        type: "varchar",
+                    },
+                    {
+                        name: "created_at",
+                        type: "timestamp",
+                        default: "now()",
+                    },
+                
+                ],
+
+            })
+        );
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("users");
     }
 
 }
