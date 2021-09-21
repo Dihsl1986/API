f6a44b9af095d6de1779aa607d05c93b1e8a59ea
Diego Santos Lima <dihsl1986@gmail.com>
2021-09-21 01:52:48 -0300

    Reiniciando o API para descoberta de erros.

--- src/repositories/SurveysRepository.ts
+++
@@ -1,9 +0,0 @@
-import { EntityRepository, Repository, } from "typeorm";
-import { Survey} from "../models/Survey";
-
-@EntityRepository(Survey)
-class surveysRepository extends Repository<Survey> {
- 
-}
-
-export{ surveysRepository };
