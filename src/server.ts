3ad1d5931847ded45637a108321a314033082d29
Diego Santos Lima <dihsl1986@gmail.com>
2021-09-22 23:13:29 -0300

    Atualizando segundo dia da nlw BANCO DE DADOS.

--- src/server.ts
+++ src/server.ts
@@ -1,28 +1,12 @@
 import "reflect-metadata";
 import express from 'express';
 import "./database";
+import { router } from "./router";
 
 const app = express();
 
-
-/**
- * GET => buscar
- * POST => salvar
- * PUT => Alterar
- * DELETE => Deletar
- * PACH => Alteração especifico
- */
-
-app.get("/", (request, response) => {
-    return response.json({message:"Hello World - NLW04"})
-});
+app.use(express.json());
+app.use(router);
 
 
-//1º PArâmetro => Rota (Recurso do API)
-//2º Parametro => request,response
-app.post("/", (request, response) => {
-    //Recebos o dados para salvar
-    return response.json({message:"Os dados foram salos co sucesso!!"})
-});
-
-app.listen(3333, () => console.log("Server is runner!!"))
+app.listen(3333, () => console.log("Server is runner!!"));
