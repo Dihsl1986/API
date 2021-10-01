11b67b5d7d52f2ad482ff7ce6933877345f0c281
Diego Santos Lima <dihsl1986@gmail.com>
2021-10-01 02:58:10 -0300

    Atualizando envio do email 4º Dia sucesso.

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,5 @@
 import {Router} from 'express';
+import { SendMailController } from './controllers/SendMailController';
 import { SurveysController } from "./controllers/SurveysController";
 import { UserController } from './controllers/UserController';
 
@@ -7,8 +8,12 @@ const router = Router();
 const userController = new UserController();
 const surveysContoller = new SurveysController();
 
+const sendMailController = new SendMailController();
+
 router.post("/users", userController.create);
 router.post("/surveys", surveysContoller.create);
 router.get("/surveys", surveysContoller.show);
+
+router.post("/sendMail", sendMailController.execute);
 
 export { router };
