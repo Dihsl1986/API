fbb8ea1a316ff3594b2d02ffea016971ce8329d3
Diego Santos Lima <dihsl1986@gmail.com>
2021-10-19 23:24:27 -0300

    Reiniciando Envio de e-mail identificando o erro e corrigindo.

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,4 @@
 import {Router} from 'express';
-import { SendMailController } from './controllers/SendMailController';
 import { SurveysController } from "./controllers/SurveysController";
 import { UserController } from './controllers/UserController';
 
@@ -8,12 +7,13 @@ const router = Router();
 const userController = new UserController();
 const surveysContoller = new SurveysController();
 
-const sendMailController = new SendMailController();
+
 
 router.post("/users", userController.create);
+
 router.post("/surveys", surveysContoller.create);
 router.get("/surveys", surveysContoller.show);
 
-router.post("/sendMail", sendMailController.execute);
+
 
 export { router };
