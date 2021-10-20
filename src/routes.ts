bda485a2ad0918bec44cdc2f4875cf56c9e53c75
Diego Santos Lima <dihsl1986@gmail.com>
2021-10-20 19:28:52 -0300

    Atualizando envio de email erro ainda não descoberto.

--- src/routes.ts
+++ src/routes.ts
@@ -1,19 +1,20 @@
-import {Router} from 'express';
+import { Router } from 'express';
+import { SendMailController } from "./controllers/SendMailController";
 import { SurveysController } from "./controllers/SurveysController";
-import { UserController } from './controllers/UserController';
+import { UserController } from "./controllers/UserController";
 
 const router = Router();
 
 const userController = new UserController();
 const surveysContoller = new SurveysController();
 
-
+const sendMailController = new SendMailController();
 
 router.post("/users", userController.create);
 
 router.post("/surveys", surveysContoller.create);
 router.get("/surveys", surveysContoller.show);
 
-
+router.post("/sendMail", sendMailController.execute);
 
 export { router };
