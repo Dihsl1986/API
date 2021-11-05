748e5f7f13d0f083e9a3fed67396fdc80dfc47e0
Diego Santos Lima <dihsl1986@gmail.com>
2021-11-05 07:43:00 -0300

    Refazendo pela 3º vez com mais cuidado.

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,34 +1,10 @@
-import { Request, Response } from 'express';
-import { getCustomRepository } from "typeorm";
-import { UsersRepository } from "../repositories/UsersRepository";
+import { Request, Response } from "express";  
 
 class UserController {
-    async create( request: Request, response: Response) {
-        const { name, email} = request.body;
-        
-        const usersRepository = getCustomRepository(UsersRepository);
 
-        //SELECT * FROM USERS WHERE EMAIL = 'EMAIL'
-        const userAlreadyExists = await usersRepository.findOne({ 
-            email,
-         });
-         //Regra de negocio para verificar se o usuário existe.
-         if (userAlreadyExists) {
-             return response.status(400).json({
-                 error: "User already exists!!!",
-             })
-         }
-
-
-        const user = usersRepository.create({
-            name, email
-        })
-
-        
-        await usersRepository.save(user);
-
-        return response.status(201).json(user);        
+    async create(request:Request, response: Response){
+    
     }
 }
 
-export { UserController };
+export { UserController }
--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,18 @@
-import { app } from "./app";
+import 'reflect-metadata';
+import express from 'express';
+import "./database"
+const app = express();
+ 
+app.get("/", (request, response) => {
+    return response.json({ message: "Hello World - NLW04"});
+});
 
-app.listen(3333, () => console.log("Server is runner!!"));
+//1º Parametro => rota(Recurso da API)
+//2º Parametro => request,response
+
+app.post("/", (request, response) => {
+    //Recebeu os dados para salvar
+    return response.json({ message: "Os dados foram salvos com sucesso!"})
+})
+
+app.listen(3333, () => console.log("Server is Running!!!"));
